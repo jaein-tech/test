52385945af537e0eda5968d677523898abbbbd22
joojaein <dvpin0102@gmail.com>
2020-02-24 20:16:07 +0900

    first practice

---
+++ simpletest/src/simpletest/first.java
@@ -0,0 +1,15 @@
+package simpletest;
+
+public class first {
+
+	public static void main(String[] args) {
+		//주석문
+		System.out.println("Hello World");
+		System.out.println("Hello");
+		System.out.println("two");
+		System.out.println("one");
+//		System.out.println("2");
+		System.out.println("주석문 단축키 Ctrl + /");
+	}
+
+}
