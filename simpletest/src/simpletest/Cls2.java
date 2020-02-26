bb0eaae8c1ac20efed32f1f202cb0fd095a41107
joojaein <dvpin0102@gmail.com>
2020-02-26 19:17:37 +0900

    참조 타입

---
+++ simpletest/src/simpletest/Cls2.java
@@ -0,0 +1,17 @@
+package simpletest;
+
+public class Cls2 {
+
+	public static void main(String[] args) {
+		// 참조타입
+		
+		// 자바의 변수 타입
+		int i = 4; // 1. 기본형 (논리형(boolean), 문자형(char), 정수형, 실수형)
+		
+		String str = new String("hello"); // 2. 참조형 기본형을 제외한 모든 타입, class, 배열 모두 참조형
+		// new String = String이라는 class를 올려라. 이렇게 메모리에 올라간 class = instance
+		// str = 메모리에 올라간 instance를 가르키는 변수, 참조하는 변수, 레퍼런스하는 변수
+		
+	}
+
+}
