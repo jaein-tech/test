c28126fdeb558e03b791909e0d2a692edb1d146b
joojaein <dvpin0102@gmail.com>
2020-02-28 17:23:55 +0900

    package

--- simpletest/src/com/testpack/simpletest/HelloPack.java
+++ simpletest/src/com/testpack/simpletest/HelloPack.java
@@ -2,5 +2,21 @@ package com.testpack.simpletest;
 // package 이름
 
 public class HelloPack {
-	// 여러가지
+//	Package
+//	관련된 class들을 모아놓은 폴더를 패키지라고 부른다
+//
+//	package packageName;
+//
+//	public class className {
+//	...
+//	}
+	
+//	패키지 이름은 대부분 도메인 이름을 거꾸로 적은 후 프로잭트 이름을 붙여 사용한다.
+//	다른 프로잭트와 헷갈리지 않기 위해서다.
+//	꼭 존재하는 도메인이 아니어도 괜찮다.
+//
+//	각 폴더명은 숫자로 시작할 수는 없다.
+//
+//	다른패키지에서 이 패키지가 가지고 있는 class를 사용할때는 import사용해서 위에 붙여준다.
+	
 }
