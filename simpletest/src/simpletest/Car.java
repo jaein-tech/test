9ff57b1a9f340c3a03426f15b7421b15e20e2e3c
joojaein <dvpin0102@gmail.com>
2020-02-28 16:55:53 +0900

    생성자 오버로딩과 this 생성자

--- simpletest/src/simpletest/Car.java
+++ simpletest/src/simpletest/Car.java
@@ -17,5 +17,16 @@ public class Car {
 	// 객체 자신을 참조하는 this
 	// this.methodName
 	
+	// 생성자 오버로딩
+	// method와 마찬가지로 매개변수의 수와 타입이 다르다면 여러개의 생성자를 선언할 수 있다.
+	public Car() {
+//		this.name = "이름없음";
+//		this.number = 0;
+		this("이름없음",0); // 위와 같음
+	}
 	
+	public Car(String name, int number) {
+		this.name = name;
+		this.number = number;
+	}
 }
